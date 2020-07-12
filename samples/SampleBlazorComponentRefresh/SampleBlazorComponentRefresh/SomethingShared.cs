4e22f0b6d1c584fad32c166ed7fe509a8601b7d5
Shady Nagy <shadynagi@gmail.com>
2020-07-12 15:05:39 +0200

    Sample added

---
+++ samples/SampleBlazorComponentRefresh/SampleBlazorComponentRefresh/SomethingShared.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleBlazorComponentRefresh
+{
+    public class SomethingShared
+    {
+        public static int CurrentCount = 0;
+    }
+}
