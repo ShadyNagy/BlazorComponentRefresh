1345c469870836fb34a150474a936c5831497d93
Shady Nagy <shadynagi@gmail.com>
2020-07-12 16:24:09 +0200

    Sample updated to use version 1.0.4

--- samples/SampleBlazorComponentRefresh/SampleBlazorComponentRefresh/Program.cs
+++ samples/SampleBlazorComponentRefresh/SampleBlazorComponentRefresh/Program.cs
@@ -20,8 +20,6 @@ namespace SampleBlazorComponentRefresh
 
             builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-            builder.Services.AddSingleton<ComponentService>();
-
             await builder.Build().RunAsync();
         }
     }
